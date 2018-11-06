2eb22be327b7188d59bf9e75e5903d9c156c6347
Mislav Teskera <mislav.teskera@gmail.com>
2018-11-06 21:02:18 +0100

    Krizaljka initial

---
+++ RjesavanjeKrizaljke/src/fitness/FitnessKrizaljka.java
@@ -0,0 +1,15 @@
+package fitness;
+
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class FitnessKrizaljka extends FitnessFunction {
+
+	@Override
+	protected double evaluate(IChromosome chromosome) {
+		// TODO Auto-generated method stub
+		// implementirati algoritam izračunavanja dobrote
+		return 0;
+	}
+
+}
